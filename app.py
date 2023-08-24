619b12bdb00c25f418fae8d978e5e2f64bcd1be0
Sobhi <s@>
2023-08-24 12:09:32 +0300

    Adjust app.py to apply the models on the database

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import os
-from flask import Flask, request, abort, jsonify
-from flask_sqlalchemy import SQLAlchemy
+
+from flask import Flask, abort, jsonify, request
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
+from .database.models import Actors, Movies, db_drop_and_create_all, setup_db
+
 
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
+  setup_db(app)
+  
+  """
+  Uncomment these to reset the database
+  """
+  # with app.app_context():
+  #   db_drop_and_create_all()
+
   CORS(app)
 
   return app
