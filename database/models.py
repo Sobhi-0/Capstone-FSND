f9d2da4f077c8320f2273810a22dfb0d1fd0614a
Sobhi <s@>
2023-08-24 11:13:59 +0300

    create the required models

---
+++ database/models.py
@@ -0,0 +1,91 @@
+import json
+import os
+
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
+
+database_path = os.environ.get('DATABASE_URL')
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+'''
+db_drop_and_create_all()
+    drops the database tables and starts fresh
+    can be used to initialize a clean database
+'''
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+
+    # Add some initial data
+    movie1 = Movies(title='Movie 1', release_date='2022-01-01')
+    movie2 = Movies(title='Movie 2', release_date='2022-02-01')
+    actor1 = Actors(name='Actor 1', age=30, gender='Male')
+    actor2 = Actors(name='Actor 2', age=25, gender='Female')
+
+    movie1.insert()
+    movie2.insert()
+    actor1.insert()
+    actor2.insert()
+
+
+class Movies(db.Model):
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    release_date = db.Column(db.Date, nullable=False)
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Actors(db.Model):
+    __tablename__ = 'actors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
