0dbfd3ae1a5f99942e733ec14109569bb23daefa
Sobhi <s@>
2023-08-25 21:09:27 +0300

    add error handlers

--- app.py
+++ app.py
@@ -77,6 +77,60 @@ def create_app(db_URI="", test_config=None):
         except:
             # If anything happened it is propably an internal error
             abort(500)
+
+
+    
+
+
+
+
+    """Error handlers for expected errors"""
+    @app.errorhandler(400)
+    def bad_request(error):
+        return (
+            jsonify({
+                "success": False,
+                "error": 400,
+                "message": "bad request"
+            }), 404)
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return (
+            jsonify({
+                "success": False,
+                "error": 404,
+                "message": "resource not found"
+            }), 404)
+
+    @app.errorhandler(405)
+    def not_found(error):
+        return (
+            jsonify({
+                "success": False,
+                "error": 405,
+                "message": "method not allowed"
+            }), 405)
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return (
+            jsonify({
+                "success": False,
+                "error": 422,
+                "message": "unprocessable"
+            }), 422)
+
+    @app.errorhandler(500)
+    def unprocessable(error):
+        return (
+            jsonify({
+                "success": False,
+                "error": 500,
+                "message": "internal server error"
+            }), 500)
+
+
     return app
 
 APP = create_app()
